6b9dedfef27d04b90e15fa7c206ec5dac2bd75b3
Jefwa10 <102663981+Jefwa10@users.noreply.github.com>
2022-04-10 23:05:39 +0300

    Add files via upload

---
+++ Discount.c
@@ -0,0 +1,20 @@
+//program to calculate discount
+
+#include<stdio.h>
+int main()
+{ 
+float discount=0.1;
+int a;
+printf("Enter a");
+scanf("%d",&a);
+if(a>5000)
+{ printf("discount is given");
+}
+else
+{
+printf("discount is not given");
+}
+return 0;
+}
+
+
---
+++ Multiplication.c
@@ -0,0 +1,20 @@
+//program to find the product of two numbers
+
+#include<stdio.h>
+
+int main()
+
+{
+int a,b,c;
+
+printf("Enter the first number\n");
+scanf("%d",&a);
+
+printf("Enter the second\n");
+scanf("%d",&b);
+
+ c=a*b;
+printf("multiply=%d",c);
+
+return 0;
+}
