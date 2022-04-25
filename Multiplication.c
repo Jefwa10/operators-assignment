2d12930f10c60d16a0c0cd0bbd86b7b37d2d0e16
Jefwa10 <102663981+Jefwa10@users.noreply.github.com>
2022-04-25 12:31:06 +0300

    Update Multiplication.c

--- Multiplication.c
+++ Multiplication.c
@@ -5,16 +5,18 @@
 int main()
 
 {
-int a,b,c;
+int num1,num2,product;
 
 printf("Enter the first number\n");
-scanf("%d",&a);
+scanf("%d",&num1);
 
 printf("Enter the second\n");
-scanf("%d",&b);
+scanf("%d",&num2);
 
- c=a*b;
-printf("multiply=%d",c);
+ product=num1*num2;
+printf("product=%d",product);
 
 return 0;
 }
+
+
